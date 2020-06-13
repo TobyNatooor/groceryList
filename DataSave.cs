9671e3ebe651490c85890458913455c7c77d348c
tobiasnf <tobiasnf@gmail.com>
2020-06-13 15:20:54 +0200

    done

---
+++ DataSave.cs
@@ -0,0 +1,72 @@
+using System;
+using System.IO;
+using foodapp;
+using System.Collections.Generic;
+
+public class DataSave
+{
+    string F;
+    public DataSave(string FileName)
+    {
+        F = FileName;
+        if (!File.Exists(FileName)) {
+            Clear();
+        }
+    }
+    public GroceryList[] GetGroceryList()
+    {
+        var All = new List<GroceryList>();
+        var AllLines = File.ReadAllLines(F);
+        for (var i = 0; i < AllLines.Length; i += 2)
+        {
+            var Grocery = new GroceryList { id = new Guid(AllLines[i]), item = AllLines[i + 1] };
+            All.Add(Grocery);
+        }
+        return All.ToArray();
+    }
+    public void AddItem(GroceryList g)
+    {
+        var a = new string[2];
+        a[0] = Guid.NewGuid().ToString();
+        a[1] = g.item;
+
+        File.AppendAllLines(F, a);
+    }
+
+    void Clear()
+    {
+        File.WriteAllText(F, "");
+    }
+    public void DeleteItem(Guid id)
+    {
+    
+        var all = GetGroceryList();
+
+        Clear();
+
+        foreach(var e in all)
+        {
+            if (e.id != id)
+            {
+            AddItem(e);
+
+            }
+        }
+
+
+        /*
+        var All = new List<string>();
+        var AllLines = File.ReadAllLines(F);
+
+        for (var i = 0; i < AllLines.Length; i += 2)
+        {
+            var Grocery = new GroceryList { id = new Guid(AllLines[i]), item = AllLines[i + 1] };
+            if (Grocery.id != id)
+            {
+                All.Add(id);
+            }
+        }
+        File.WriteAllLines(F, All.ToArray());
+        */
+    }
+}
